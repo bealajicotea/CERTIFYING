bf7f8ddc3298fff6aad803e43603cb81735be50b
beatriz <beatrizsalazarbaez@gmail.com>
2025-06-15 20:14:00 -0400

    reporte

--- certificacion/views/teacher_views.py
+++ certificacion/views/teacher_views.py
@@ -5,6 +5,7 @@ from apps.convocatorias.models import Convocatoria
 from apps.inscripciones.models import Inscripcion
 from apps.resultados.models import Resultado
 from django.db import models
+from django.core.paginator import Paginator
 
 def index_view(request):
     return render(request, 'pagina_principal.html')
@@ -19,6 +20,11 @@ def reporte_mensual_view(request):
     tipo_convocatoria = request.GET.get('tipo_convocatoria', '')
     estado_inscripcion = request.GET.get('estado_inscripcion', '')
     nivel = request.GET.get('nivel', '')
+
+    # Todas las opciones posibles
+    TODOS_TIPOS = [t[0] for t in Convocatoria.TIPO_CONVOCATORIA]
+    TODOS_NIVELES = [n[0] for n in Convocatoria.niveles]
+    TODOS_ESTADOS = list(Inscripcion.objects.values_list('estado', flat=True).distinct())
 
     # Filtrar convocatorias
     convocatorias = Convocatoria.objects.filter(fecha_creacion__date__gte=inicio_mes, fecha_creacion__date__lte=fin_mes)
@@ -53,15 +59,37 @@ def reporte_mensual_view(request):
         )
     resultados_por_nivel = resultados.values('nota').order_by('nota').annotate(total=models.Count('id'))
 
+    # Paginación (6 por página para cada tabla)
+    page_c = request.GET.get('page_c', 1)
+    page_i = request.GET.get('page_i', 1)
+    page_ic = request.GET.get('page_ic', 1)
+    page_r = request.GET.get('page_r', 1)
+
+    paginator_c = Paginator(list(convocatorias_por_tipo), 6)
+    paginator_i = Paginator(list(inscripciones_por_estado), 6)
+    paginator_ic = Paginator(list(inscripciones_por_convocatoria), 6)
+    paginator_r = Paginator(list(resultados_por_nivel), 6)
+
     contexto = {
         'convocatorias_total': convocatorias.count(),
-        'convocatorias_por_tipo': list(convocatorias_por_tipo),
+        'convocatorias_por_tipo': paginator_c.get_page(page_c),
         'inscripciones_total': inscripciones.count(),
-        'inscripciones_por_estado': list(inscripciones_por_estado),
-        'inscripciones_por_convocatoria': list(inscripciones_por_convocatoria),
+        'inscripciones_por_estado': paginator_i.get_page(page_i),
+        'inscripciones_por_convocatoria': paginator_ic.get_page(page_ic),
         'resultados_total': resultados.count(),
-        'resultados_por_nivel': list(resultados_por_nivel),
+        'resultados_por_nivel': paginator_r.get_page(page_r),
         'inicio_mes': inicio_mes,
         'fin_mes': fin_mes,
+        'todos_tipos': TODOS_TIPOS,
+        'todos_niveles': TODOS_NIVELES,
+        'todos_estados': TODOS_ESTADOS,
+        'selected_tipo_convocatoria': tipo_convocatoria,
+        'selected_nivel': nivel,
+        'selected_estado_inscripcion': estado_inscripcion,
+        'buscar': buscar,
+        'paginator_c': paginator_c,
+        'paginator_i': paginator_i,
+        'paginator_ic': paginator_ic,
+        'paginator_r': paginator_r,
     }
     return render(request, 'reporte_mensual.html', contexto)
